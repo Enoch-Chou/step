a9081d40e36384617175df0f4d73a587504324f8
Enoch Chou <enochs.chou@gmail.com>
2020-07-06 17:34:35 +0000

    Add createMap function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -80,3 +80,10 @@ function drawChart() {
         chart.draw(data, options);
     });
 }
+
+function createMap() {
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {center: {lat: 37.422, lng: -122.084}, zoom:16}
+    );
+}
